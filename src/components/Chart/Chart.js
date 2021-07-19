277c68717b687e23955c15fd872bb1db3282c43b
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-07-18 21:11:47 -0700

    Chart changes color after API call

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -9,52 +9,34 @@ const green = ["rgb(138,246, 138,.85)", "rgb(138,246, 138,.25)", "rgb(138,246, 1
 const green2 = ["rgb(175, 243, 173,.85)", "rgb(175, 243, 173,.25)", "rgb(175, 243, 173,.15)"]; //#AFF3AD
 let color=black
 
-const GlowLine = [
-  <Line dataKey="time_open" type="monotone" animationDuration={0} stroke="transparent" dot={false} key={0} />,
-  <Line dataKey="rate_open" animationDuration={2200} stroke={color[0]} dot={false} key={1} />,
-  <Line
-    dataKey="rate_open"
-    type="monotone"
-    strokeWidth={2}
-    animationDuration={2200}
-    stroke={color[1]}
-    dot={false}
-    key={2}
-  />,
-  <Line
-    dataKey="rate_open"
-    type="monotone"
-    strokeWidth={4}
-    animationDuration={2200}
-    stroke={color[2]}
-    dot={false}
-    key={3}
-  />,
-];
+
 
 class Chart extends React.Component {
   state = {
     chartData: Data,
-    token: this.props.symbol
+    token: this.props.symbol,
+    color:color
   }
   componentDidMount() {
     apiClient.getCoinThreeMonthPriceHistory(this.props.symbol).then(res => {
+      let open = res.data[0].rate_open;
+      let close = res.data[res.data.length - 1].rate_open;
+      color = black;
+  
+  
+      if (open > close) {
+        color = red;
+      }
+      if (close > open) {
+        color = green;
+      }
+    
       this.setState({
-        chartData:res.data
+        chartData:res.data,
+        token: this.props.symbol,
+        color:color
       })
     })
-    let open = Data[0].rate_open;
-    let close = Data[Data.length - 1].rate_open;
-    color = black;
-
-    // close =.0001
-
-    if (open > close) {
-      color = red;
-    }
-    if (close > open) {
-      color = green;
-    }
   }
 
 
@@ -63,7 +45,10 @@ class Chart extends React.Component {
     return (
       <ResponsiveContainer width="75%" height={500}>
         <LineChart width={600} height={300} data={this.state.chartData}>
-          {GlowLine}
+        <Line dataKey="time_open" type="monotone" animationDuration={0} stroke="transparent" dot={false} key={0} />
+        <Line dataKey="rate_open" animationDuration={2200} stroke={color[0]} dot={false} key={1} />
+        <Line dataKey="rate_open" type="monotone" strokeWidth={2} animationDuration={2200} stroke={color[1]} dot={false} key={2}/>
+        <Line dataKey="rate_open" type="monotone" strokeWidth={4} animationDuration={2200} stroke={color[2]} dot={false} key={3}/>
           <YAxis dataKey="rate_open" stroke="transparent" />
           <XAxis dataKey="time_open" stroke="transparent" />
           <Tooltip content={CustomTooltip} />

26bac7edc2e46ebcfcf91c28922ef03194a115a3
vZireael <75550025+vZireael@users.noreply.github.com>
2021-07-30 16:18:11 -0400

    added responsiveness to dashboard and fixed card buttons

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import btc from "../../images/btc.png";
@@ -8,13 +8,24 @@ import doge from "../../images/doge.png";
 import dot from "../../images/dot.png";
 import xmr from "../../images/xmr.png";
 
-const CoinCard = ({ name, symbol, amount, color, id }) => {
+const CoinCard = ({ name, symbol, amount, color, id, setSymbol, setName }) => {
   const useStyles = makeStyles({
     card: {
-      width: "370px",
+      width: "24rem",
       height: "15.4rem",
       background: color,
       borderRadius: "5px",
+      "@media (max-width: 1560px)": {
+        width: "18.5rem",
+        height: "10rem",
+        marginBottom: -22,
+      },
+      "@media (max-width: 1400px)": {
+        width: "24rem",
+        height: "12rem",
+        background: color,
+        borderRadius: "5px",
+      },
     },
     title: {
       marginLeft: 20,
@@ -29,11 +40,20 @@ const CoinCard = ({ name, symbol, amount, color, id }) => {
       alignItems: "center",
       justifyContent: "space-evenly",
       marginTop: "2.5rem",
+      "@media (max-width: 1560px)": {
+        marginTop: 0,
+      },
+      "@media (max-width: 1400px)": {
+        marginTop: "1rem",
+      },
     },
     image: {
       height: "70px",
       marginLeft: 10,
       marginTop: 10,
+      "@media (max-width: 1560px)": {
+        height: "60px",
+      },
     },
     imgShadow: {
       borderRadius: "50%",
@@ -41,6 +61,14 @@ const CoinCard = ({ name, symbol, amount, color, id }) => {
       opacity: 0.8,
       height: "90px",
       width: "90px",
+      "@media (max-width: 1560px)": {
+        height: "80px",
+        width: "80px",
+      },
+    },
+    link: {
+      textDecoration: "none",
+      color: "white",
     },
   });
 
@@ -50,21 +78,30 @@ const CoinCard = ({ name, symbol, amount, color, id }) => {
 
   return (
     <div className={classes.card}>
-      <div className="title">
-        <Typography variant="h6" className={classes.title}>
-          {name}
-        </Typography>
-      </div>
-      <div className={classes.mainCard}>
-        <div className={classes.imgShadow}>
-          <img src={images[id - 1]} alt={name} className={classes.image} />
+      <Link
+        className={classes.link}
+        to={"/coin/" + symbol}
+        onClick={() => {
+          setSymbol(symbol);
+          setName(name);
+        }}
+      >
+        <div className="title">
+          <Typography variant="h6" className={classes.title}>
+            {name}
+          </Typography>
         </div>
-        <div className={classes.info}>
-          <Typography variant="h6">{symbol}</Typography>
-          <Typography variant="h6">{amount.toLocaleString()}</Typography>
+        <div className={classes.mainCard}>
+          <div className={classes.imgShadow}>
+            <img src={images[id - 1]} alt={name} className={classes.image} />
+          </div>
+          <div className={classes.info}>
+            <Typography variant="h6">{symbol}</Typography>
+            <Typography variant="h6">{amount.toLocaleString()}</Typography>
+          </div>
+          <div className="marketData"></div>
         </div>
-        <div className="marketData"></div>
-      </div>
+      </Link>
     </div>
   );
 };
